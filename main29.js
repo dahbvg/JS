424291ff87fcfe3964f5bc2dc67e118641f01354
dahbvg <wiktor.golec@gmail.com>
2022-09-06 13:06:48 +0100

    29-js-suma-zmiennych

---
+++ main29.js
@@ -0,0 +1,16 @@
+
+let firstNumer = 5;
+
+let secondNumber = 14;
+
+function suma(firstNumer, secondNumber) {
+    let wynik = firstNumer + secondNumber;
+
+    return wynik;
+}
+
+console.log(suma (firstNumer, secondNumber));
+
+let dodowanie = suma(firstNumer, secondNumber);
+
+console.log(dodowanie);
