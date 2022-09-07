3bbbf1ddb0f96e61c35ec3388c7c2c07dab8ef46
dahbvg <wiktor.golec@gmail.com>
2022-09-07 22:04:14 +0100

    34-js-dlugosc-stringa

---
+++ main34.js
@@ -0,0 +1,16 @@
+let ex1 = 'Uwielbiam JavaScript'
+let ex2 = 'Jestem świetnym programistą'
+
+
+function porownanie(exp1, exp2) {
+    if (exp1.length > exp2.length) {
+        console.log(exp1)
+    } else {
+        console.log(exp2)
+    }
+}
+
+
+
+
+porownanie(ex1, ex2)
