b46dc478784d0c01ffc42482c529891c99fe642e
dahbvg <wiktor.golec@gmail.com>
2022-09-09 13:11:51 +0100

    38-js-dzialania

--- main38.js
+++ main38.js
@@ -6,11 +6,34 @@ console.log(en2)
 
 function twenty(n1, n2) {
 
-    console.log('Wynik dodawania wynosi ' + (n1 + n2));
+    if ((n1 + n2 > 0) && (n1 - n2 > 0) && (n1 * n2)) {
 
-    console.log('Wynik odejmowania wynosi ' + (n1 - n2));
+        console.log('Wynik dodawania wynosi ' + (n1 + n2));
 
-    console.log('Wynik mnożenia wynosi ' + (n1 * n2));
+        console.log('Wynik odejmowania wynosi ' + (n1 - n2));
+
+        console.log('Wynik mnożenia wynosi ' + (n1 * n2));
+    } else {
+        console.log('Wynik jest nieprawidłowy');
+    }
 }
-
 twenty(en1, en2)
+
+
+function twentyvol1(n1, n2) {
+
+    if (n1 + n2 > 0) {
+        console.log('Wynik dodawania wynosi ' + (n1 + n2));
+    }
+    
+    if (n1 - n2 > 0) {
+        console.log('Wynik odejmowania wynosi ' + (n1 - n2));
+    } else if (n1 - n2 < 0) {
+        console.log('Wynik jest nieprawidłowy');
+    }
+
+    if (n1 * n2 > 0) {
+        console.log('Wynik mnożenia wynosi ' + (n1 * n2));
+    }
+}
+twentyvol1(en1, en2)
