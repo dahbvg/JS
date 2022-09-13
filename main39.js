5ed6cfe7494b6ceb2826b3e4d8f8eb020b3b26fc
dahbvg <wiktor.golec@gmail.com>
2022-09-13 13:54:57 +0100

    39-js-elementy-tablicy

---
+++ main39.js
@@ -0,0 +1,19 @@
+let en1 = Math.floor(Math.random() * 21)
+let en2 = Math.floor(Math.random() * 21)
+let en3 = Math.floor(Math.random() * 21)
+let en4 = Math.floor(Math.random() * 21)
+let en5 = Math.floor(Math.random() * 21)
+
+console.log(en1)
+console.log(en2)
+console.log(en3)
+console.log(en4)
+console.log(en5)
+
+let tablica = [en1, en2, en3, en4, en5]
+
+function zwrot (dowolna_tablica){
+    console.log(dowolna_tablica[0])
+}
+
+zwrot (tablica)
